c3c9ba9955c69ec79a47c133ef393ad1e4fac890
Alex Kapranoff <alex@kapranoff.ru>
2020-12-01 23:24:52 -0800

    2.1

--- 02/main2.cpp
+++ 02/main2.cpp
@@ -8,13 +8,12 @@ int main()
 
   int valid_passwords = 0;
   while (std::getline(std::cin, line)) {
-    int min = std::stoi(line);
-    int max = std::stoi(std::string(line.begin() + line.find('-') + 1, line.end()));
+    int first = std::stoi(line);
+    int second = std::stoi(std::string(line.begin() + line.find('-') + 1, line.end()));
     char c = line[line.find(':') - 1];
     std::string password = line.substr(line.rfind(' ') + 1);
 
-    int count = std::count(password.begin(), password.end(), c);
-    if (count >= min && count <= max) {
+    if ((password[first - 1] == c) != (password[second - 1] == c)) {
       ++valid_passwords;
     }
   }
