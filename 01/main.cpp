ed0aa928f4a57550215acb9451de2129a2a3dfca
Alex Kapranoff <alex@kapranoff.ru>
2020-12-01 17:55:31 -0800

    Import 1.1

---
+++ 01/main.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  vector<int> entries;
+
+  int entry;
+  while (cin >> entry) {
+    entries.push_back(entry);
+  }
+
+  entries.sort(entries.begin(), entries.end());
+
+  for(auto first : entries) {
+    if (binary_search(entries.begin(), entries.end(), 2020 - first)) {
+      cout << first * (2020 - first) << '\n';
+    }
+  }
+}
