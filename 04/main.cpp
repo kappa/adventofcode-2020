709d00e5748bbe2063e107ff43d929965103b1ba
Alex Kapranoff <alex@kapranoff.ru>
2020-12-05 01:06:34 -0800

    Day 4 clean up

--- 04/main.cpp
+++ 04/main.cpp
@@ -2,33 +2,37 @@
 #include <unordered_set>
 #include <sstream>
 
+using passport_t = std::unordered_set<std::string>;
+
+bool is_valid(const passport_t& cur_passport) {
+  const static passport_t seven_fields{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
+
+  return cur_passport == seven_fields;
+}
+
 int main()
 {
   int valid = 0;
 
-  std::unordered_set<std::string> seven_fields{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
   std::unordered_set<std::string> cur_passport{};
 
-  std::string line = "";
+  std::string line;
 
   while(std::getline(std::cin, line)) {
     if (line.empty()) {
-      cur_passport.extract("cid");
-      if (cur_passport == seven_fields)
-        ++valid;
+      if (is_valid(cur_passport)) ++valid;
       cur_passport.clear();
     } else {
       std::istringstream words(line);
-      std::string word = "";
+      std::string word;
       while (words >> word) {
-        cur_passport.insert(word.substr(0, 3));
+        const auto key = word.substr(0, 3);
+        if (key != "cid") cur_passport.insert(key);
       }
     }
   }
 
-  cur_passport.extract("cid");
-  if (cur_passport == seven_fields)
-    ++valid;
+  if (is_valid(cur_passport)) ++valid;
 
   std::cout << valid << "\n";
 
--- 04/main2.cpp
+++ 04/main2.cpp
@@ -8,51 +8,56 @@
 
 using passport_t = std::unordered_map<std::string, std::string>;
 
-bool valid_values(passport_t passport) {
-  const std::regex yr_re("\\d{4}");
+bool valid_year(const std::string& str, int min, int max) {
+  const static std::regex yr_re("\\d{4}");
+
+  std::smatch m;
+  if (std::regex_match(str, m, yr_re)) {
+    const int value = std::stoi(m[0]);
+    return value >= min && value <= max;
+  }
+
+  return false;
+}
+
+bool valid_values(const passport_t& passport) {
+  if (!valid_year(passport.at("byr"), 1920, 2002))
+    return false;
+  if (!valid_year(passport.at("iyr"), 2010, 2020))
+    return false;
+  if (!valid_year(passport.at("eyr"), 2020, 2030))
+    return false;
+
   const std::regex hgt_re("(\\d+)(in|cm)");
+  std::smatch m;
+  if (std::regex_match(passport.at("hgt"), m, hgt_re)) {
+    const int value = std::stoi(m[1]);
+    if (m[2] == "in" && (value < 59 || value > 76))
+      return false;
+    if (m[2] == "cm" && (value < 150 || value > 193))
+      return false;
+  } else
+    return false;
+
   const std::regex hcl_re("#[0-9a-f]{6}");
   const std::regex ecl_re("amb|blu|brn|gry|grn|hzl|oth");
   const std::regex pid_re("[0-9]{9}");
-
-  std::smatch m;
-
-  if (std::regex_match(passport["byr"], m, yr_re)) {
-    int value = std::stoi(m[0]);
-    if (value < 1920 || value > 2002)
-      return false;
-  }
-
-  if (std::regex_match(passport["iyr"], m, yr_re)) {
-    int value = std::stoi(m[0].str());
-    if (value < 2010 || value > 2020)
-      return false;
-  }
-
-  if (std::regex_match(passport["eyr"], m, yr_re)) {
-    int value = std::stoi(m[0].str());
-    if (value < 2020 || value > 2030)
-      return false;
-  }
-
-  if (std::regex_match(passport["hgt"], m, hgt_re)) {
-    int value = std::stoi(m[1].str());
-    if (m[2].str() == "in" && (value < 59 || value > 76))
-      return false;
-    if (m[2].str() == "cm" && (value < 150 || value > 193))
-      return false;
-  } else {
-    return false;
-  }
-
-  if (!std::regex_match(passport["hcl"], hcl_re))
-    return false;
-  if (!std::regex_match(passport["ecl"], ecl_re))
-    return false;
-  if (!std::regex_match(passport["pid"], pid_re))
+  if (!std::regex_match(passport.at("hcl"), hcl_re)
+      || !std::regex_match(passport.at("ecl"), ecl_re)
+      || !std::regex_match(passport.at("pid"), pid_re))
     return false;
 
   return true;
+}
+
+bool is_valid(const passport_t& cur_passport) {
+  const static std::unordered_set<std::string> seven_fields{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
+
+  std::unordered_set<std::string> cur_fields{};
+  for(const auto& pair : cur_passport)
+    cur_fields.insert(pair.first);
+
+  return cur_fields == seven_fields && valid_values(cur_passport);
 }
 
 int main()
@@ -62,35 +67,25 @@ int main()
   std::unordered_set<std::string> seven_fields{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
   passport_t cur_passport{};
 
-  std::string line = "";
+  std::string line;
 
   while(std::getline(std::cin, line)) {
     if (line.empty()) {
-      cur_passport.extract("cid");
-
-      std::unordered_set<std::string> cur_fields{};
-      for(const auto& pair : cur_passport)
-        cur_fields.insert(pair.first);
-
-      if (cur_fields == seven_fields && valid_values(cur_passport))
+      if (is_valid(cur_passport))
         ++valid;
       cur_passport.clear();
     } else {
       std::istringstream words(line);
       std::string word = "";
       while (words >> word) {
-        cur_passport[word.substr(0, 3)] = word.substr(4);
+        const auto key = word.substr(0, 3);
+        if (key != "cid")
+          cur_passport[key] = word.substr(4);
       }
     }
   }
 
-  cur_passport.extract("cid");
-
-  std::unordered_set<std::string> cur_fields{};
-  for(const auto& pair : cur_passport)
-    cur_fields.insert(pair.first);
-
-  if (cur_fields == seven_fields && valid_values(cur_passport))
+  if (is_valid(cur_passport))
     ++valid;
 
   std::cout << valid << "\n";
