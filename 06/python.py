52668d2863cca2e02b03bbe23605b6826fa2c7e1
Alex Kapranoff <alex@kapranoff.ru>
2020-12-06 11:07:02 -0800

    Remove results

--- 06/python.py
+++ 06/python.py
@@ -11,9 +11,9 @@ answers = list(map(lambda x: [set(i) for i in x.split('\n') if i != ''], raw))
 # %%
 ### part 1
 n_unique = map(lambda x: len(set.union(*x)), answers)
-print(sum(n_unique)) #6530
+print(sum(n_unique))
 
 # %%
 ### part 2
 n_intersect = map(lambda x: len(set.intersection(*x)), answers)
-print(sum(n_intersect)) #3323
+print(sum(n_intersect))
