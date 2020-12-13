07ecf273b1c344ed7530f3e84f4931a0a1123126
Alex Kapranoff <alex@kapranoff.ru>
2020-12-13 12:34:05 -0800

    Clean up 2 implementations of part 2
    Play with accumulates. Ignore chars in cin.

--- 13/main2.cpp
+++ 13/main2.cpp
@@ -7,13 +7,13 @@
 #include <vector>
 
 constexpr
-long long modulo(const long long a, const long long b) {
+long modulo(const long a, const long b) {
   const auto result = a % b;
   return result >= 0 ? result : result + b;
 }
 
 constexpr
-long long power_mod(const long long base, const int power, const int mod) {
+long power_mod(const long base, const int power, const int mod) {
   auto rv = modulo(base, mod);
 
   for (auto i = 0; i < power - 1; ++i)
@@ -24,41 +24,42 @@ long long power_mod(const long long base, const int power, const int mod) {
 
 // Fermat's little theorem
 constexpr
-long long mulinv_mod(const long long num, const int mod) {
+long mulinv_mod(const long num, const int mod) {
   return power_mod(num, mod - 2, mod);
 }
 
 int main()
 {
-  std::string line;
-  std::getline(std::cin, line);
-  std::getline(std::cin, line);
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
   struct bus_t {
     int a, r;
   };
 
   std::vector<bus_t> buses;
-  std::istringstream is(line);
+
   std::string w;
-  for(int i = 0; std::getline(is, w, ','); ++i) {
-    if (w != "x")
-      buses.push_back(bus_t{std::stoi(w), i % std::stoi(w)});
+  for(int i = 0; std::getline(std::cin, w, ','); ++i) {
+    if (w != "x") {
+      const auto num = std::stoi(w);
+      // "i" is remainder, so if it's > num, mod it right away
+      buses.push_back(bus_t{num, i % num});
+    }
   }
 
-  long long m = 1;
-  for(auto bus : buses)
-    m *= bus.a;
+  const long m =
+    std::accumulate(buses.begin(), buses.end(), 1l, [](const long acc, const bus_t& bus) {
+      return acc * bus.a;
+    });
 
-  long long x = 0;
-  for (auto bus : buses) {
+  const long x = std::accumulate(buses.begin(), buses.end(), 0l, [m](const long acc, const bus_t &bus) {
     const auto mi = m / bus.a;
     const auto minv = mulinv_mod(mi, bus.a);
 
-    x = modulo(x + (bus.a - bus.r) * mi * minv, m);
-  }
+    return modulo(acc + (bus.a - bus.r) * mi * minv, m);
+  });
 
-  std::cout << modulo(x, m) << std::endl;
+  std::cout << x << std::endl;
 
   return 0;
 }
--- 13/main3.cpp
+++ 13/main3.cpp
@@ -8,25 +8,25 @@
 
 int main()
 {
-  std::string line;
-  std::getline(std::cin, line);
-  std::getline(std::cin, line);
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
   struct bus_t {
     int a, r;
   };
 
   std::vector<bus_t> buses;
-  std::istringstream is(line);
+
   std::string w;
-  for(int i = 0; std::getline(is, w, ','); ++i) {
-    if (w != "x")
-      buses.push_back(bus_t{std::stoi(w), i % std::stoi(w)});
+  for(int i = 0; std::getline(std::cin, w, ','); ++i) {
+    if (w != "x") {
+      const auto num = std::stoi(w);
+      // "i" is remainder, so if it's > num, mod it right away
+      buses.push_back(bus_t{num, i % num});
+    }
   }
 
   long t = buses[0].a;
   long step = t;
-
   for (auto i = 1; i < (int)buses.size(); ++i) {
     while (t % buses[i].a != (buses[i].a - buses[i].r))
       t += step;
