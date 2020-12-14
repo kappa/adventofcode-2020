7a8b04d9ef98bdc4ac116ad02416d3e8c8b3b5b4
Alex Kapranoff <alex@kapranoff.ru>
2020-12-13 20:39:44 -0800

    Reformat and clean day 12

--- 12/main.cpp
+++ 12/main.cpp
@@ -1,31 +1,52 @@
-#include <cstdlib>
 #include <iostream>
 
-int main()
-{
-  std::string line;
+int main() {
   int x = 0, y = 0;
   int dir = 90;
 
-  while(std::getline(std::cin, line)) {
+  for (std::string line; std::getline(std::cin, line);) {
     const char c = line[0];
     const int num = std::stoi(line.substr(1));
 
-    switch(c) {
-    case 'N': y += num; break;
-    case 'S': y -= num; break;
-    case 'E': x += num; break;
-    case 'W': x -= num; break;
-    case 'L': dir = (dir + 360 - num) % 360; break;
-    case 'R': dir = (dir + num) % 360; break;
-    case 'F': switch (dir) {
-      case 0: y += num; break;
-      case 90: x += num; break;
-      case 180: y -= num; break;
-      case 270: x -= num; break;
-      default: std::cerr << "invalid dir\n";
-      }; break;
-    default: std::cerr << "invalid command\n";
+    switch (c) {
+    case 'N':
+      y += num;
+      break;
+    case 'S':
+      y -= num;
+      break;
+    case 'E':
+      x += num;
+      break;
+    case 'W':
+      x -= num;
+      break;
+    case 'L':
+      dir = (dir + 360 - num) % 360;
+      break;
+    case 'R':
+      dir = (dir + num) % 360;
+      break;
+    case 'F':
+      switch (dir) {
+      case 0:
+        y += num;
+        break;
+      case 90:
+        x += num;
+        break;
+      case 180:
+        y -= num;
+        break;
+      case 270:
+        x -= num;
+        break;
+      default:
+        std::cerr << "invalid dir\n";
+      };
+      break;
+    default:
+      std::cerr << "invalid command\n";
     }
   }
 
--- 12/main2.cpp
+++ 12/main2.cpp
@@ -1,37 +1,47 @@
-#include <cstdlib>
 #include <iostream>
 
 void left(int &wx, int &wy) {
-  auto t = wx;
+  const auto t = wx;
   wx = -wy;
   wy = t;
 }
 
-void right(int &wx, int &wy) {
-  auto t = wx;
-  wx = wy;
-  wy = -t;
-}
-
-int main()
-{
-  std::string line;
+int main() {
   int x = 0, y = 0;
   int wx = 10, wy = 1;
 
-  while(std::getline(std::cin, line)) {
+  for (std::string line; std::getline(std::cin, line);) {
     const char c = line[0];
     const int num = std::stoi(line.substr(1));
 
-    switch(c) {
-    case 'N': wy += num; break;
-    case 'S': wy -= num; break;
-    case 'E': wx += num; break;
-    case 'W': wx -= num; break;
-    case 'L': for(auto i = 0; i < num / 90; ++i) left(wx, wy); break;
-    case 'R': for(auto i = 0; i < num / 90; ++i) right(wx, wy); break;
-    case 'F': x += num * wx; y += num * wy; break;
-    default: std::cerr << "invalid command\n";
+    switch (c) {
+    case 'N':
+      wy += num;
+      break;
+    case 'S':
+      wy -= num;
+      break;
+    case 'E':
+      wx += num;
+      break;
+    case 'W':
+      wx -= num;
+      break;
+    case 'L':
+      for (auto i = 0; i < num / 90; ++i)
+        left(wx, wy);
+      break;
+    case 'R':
+      // implement right turl as left by 360-deg angle
+      for (auto i = 0; i < (360 - num) / 90; ++i)
+        left(wx, wy);
+      break;
+    case 'F':
+      x += num * wx;
+      y += num * wy;
+      break;
+    default:
+      std::cerr << "invalid command\n";
     }
   }
 
